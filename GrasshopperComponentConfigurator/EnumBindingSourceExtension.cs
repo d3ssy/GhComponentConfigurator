d946061beea69714d0eba29538ce405f073db0f5
Guido Maciocci <guido.maciocci@lendlease.com>
2021-09-21 10:21:51 +1000

    Add enum binding extension.

---
+++ GrasshopperComponentConfigurator/EnumBindingSourceExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Markup;
+
+namespace GrasshopperComponentConfigurator
+{
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        public Type EnumType { get; set; }
+
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            if (enumType is null || !enumType.IsEnum)
+            {
+                throw new ArgumentException("EnumType must not be null and must be of type Enum.");
+            }
+
+            EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType);
+        }
+    }
+}
